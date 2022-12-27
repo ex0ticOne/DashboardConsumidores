22967b6c18acf7354adc484284279db0afe1b0a8
ex0ticOne <rricardorb@gmail.com>
2022-12-27 13:38:43 -0300

    novo app

--- app.R
+++ app.R
@@ -11,21 +11,35 @@ cabecalho <- dashboardHeader(title = "Dashboard analítico de reclamações",
                              titleWidth = "500")
 
 #Lateral
-lateral <- dashboardSidebar(sidebarMenu(uiOutput("selecao_segmento_mercado")), 
+lateral <- dashboardSidebar(sidebarMenu(uiOutput("selecao_segmento_mercado"),
+                                        menuItem("Panorama Geral", tabName = "panorama_geral"),
+                                        menuItem("Indicadores", tabName = "indicadores")), 
                             width = "300")
 
 #Corpo
-corpo <- dashboardBody(tabBox(tabPanel(title = "Resumo Analítico por Empresa do Segmento",
-                                       DTOutput("resumo_analitico")),
-                              tabPanel(title = "Perfil Reclamante",
-                                       DTOutput("perfil_reclamante")),
-                              tabPanel(title = "Principais Problemas",
-                                       DTOutput("principais_problemas")),
-                              tabPanel(title = "Meio de Contratação",
-                                       DTOutput("meio_contratacao")), width = "100%", height = "100%"))
+corpo <- dashboardBody(tabItems(tabItem(tabName = "panorama_geral",
+                                        fluidRow(box(titlePanel("Esse é o panorama geral do segmento selecionado"),
+                                                     valueBoxOutput("media_nota_mercado", width = "6"),
+                                                     valueBoxOutput("indice_geral_respostas", width = "6"),
+                                                     valueBoxOutput("tempo_medio_respostas_mercado", width = "6"),
+                                                     valueBoxOutput("indice_geral_resolucao", width = "6"), width = "100%"))),
+                                tabItem(tabName = "indicadores", 
+                                        tabBox( 
+                                          tabPanel(title = "Resumo Analítico por Empresa do Segmento",
+                                                   titlePanel("Esse é o ranking geral por empresa do segmento selecionado"),
+                                                   DTOutput("resumo_analitico")),
+                                          tabPanel(title = "Perfil Reclamante",
+                                                   titlePanel("Quais os perfis de quem está reclamando nesse segmento?"),
+                                                   DTOutput("perfil_reclamante")),
+                                          tabPanel(title = "Principais Problemas",
+                                                   titlePanel("Quais os principais problemas enfrentados pelos clientes?"),
+                                                   DTOutput("principais_problemas")),
+                                          tabPanel(title = "Meio de Contratação",
+                                                   titlePanel("Quais os canais de contratação dos produtos/serviços que geraram reclamações?"),
+                                                   DTOutput("meio_contratacao")), width = "100%", height = "100%"))))
 
 ui <- dashboardPage(cabecalho, lateral, corpo, skin = "green", 
-                    title = "Dashboard")
+                    title = "Dashboard de Consumidores")
 
 server <- function(input, output) {
   
@@ -43,6 +57,43 @@ server <- function(input, output) {
     
   })
   
+  criacao_media_nota <- reactive({
+    
+    media_nota_mercado <- round(mean(dataset_segmento()$nota_do_consumidor, na.rm = TRUE), digits = 2)
+    
+  })
+  
+  output$media_nota_mercado <- renderValueBox(valueBox(subtitle = "Média da Nota do Consumidor (de 1 a 5)", 
+                                                       value = criacao_media_nota(), color = "blue"))
+  
+  criacao_indice_geral_respostas <- reactive({
+    
+    indice_geral_respostas <- round(sum(dataset_segmento()$respondida) / length(dataset_segmento()$respondida) * 100, digits = 2)
+    
+  })
+  
+  output$indice_geral_respostas <- renderValueBox(valueBox(subtitle = "Índice Geral de Respostas",
+                                                           value = criacao_indice_geral_respostas(), color = "olive"))
+  
+  criacao_tempo_medio_respostas_mercado <- reactive({
+    
+    tempo_medio_respostas <- round(mean(dataset_segmento()$tempo_resposta, na.rm = TRUE), digits = 2)
+    
+  })
+  
+  output$tempo_medio_respostas_mercado <- renderValueBox(valueBox(subtitle = "Tempo Médio de Respostas em Dias", 
+                                                                  value = criacao_tempo_medio_respostas_mercado(), color = "maroon"))
+  
+  criacao_indice_geral_resolucao <- reactive({
+    
+    indice_geral_resolucao <- round(sum(dataset_segmento()$resolveu_reclamacao) / length(dataset_segmento()$resolveu_reclamacao) * 100, digits = 2)
+    
+  })
+  
+  output$indice_geral_resolucao <- renderValueBox(valueBox(subtitle = "Índice Geral de Resolução de Acordo com o Cliente", 
+                                                           value = criacao_indice_geral_resolucao(), color = "orange"))
+  
+  #Resumo Analítico
   criacao_resumo <- reactive({
     
     resumo_analitico_empresas <- dataset_segmento() %>%
@@ -87,18 +138,22 @@ server <- function(input, output) {
   })
   
   output$resumo_analitico <- renderDT(criacao_resumo(), 
+                                      filter = list(position = 'top'),
                                       options = list(language = list(url = '//cdn.datatables.net/plug-ins/1.13.1/i18n/pt-BR.json')),
                                       colnames = c('Nome da Empresa', 'Quantidade de Reclamações', 'Prazo Médio para Resposta em Dias', 'Média da Nota', 'Índice de Respostas da Empresa', 'Índice de Resolução de acordo com o cliente'))
   
   output$perfil_reclamante <- renderDT(criacao_perfil_reclamante(), 
+                                       filter = list(position = 'top'),
                                        options = list(language = list(url = '//cdn.datatables.net/plug-ins/1.13.1/i18n/pt-BR.json')),
                                        colnames = c('UF', 'Cidade', 'Faixa Etária', 'Sexo', 'Quantidade'))
   
   output$principais_problemas <- renderDT(criacao_principais_problemas(), 
+                                          filter = list(position = 'top'),
                                              options = list(language = list(url = '//cdn.datatables.net/plug-ins/1.13.1/i18n/pt-BR.json')),
-                                             colnames = c('Problema', 'Quantidade de Ocorrências'))
+                                             colnames = c('Problema', 'Quantidade'))
   
   output$meio_contratacao <- renderDT(criacao_meio_contratacao(), 
+                                      filter = list(position = 'top'),
                                          options = list(language = list(url = '//cdn.datatables.net/plug-ins/1.13.1/i18n/pt-BR.json')), 
                                          colnames = c('Meio de Contratação', 'Quantidade'))
   
